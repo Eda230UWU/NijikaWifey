f884c539b3729923ae8f9137bf00bfd02b89ec1c
eda uwu <edafru@gmail.com>
2023-05-22 10:58:32 +0200

    import

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,8 @@
 <script>
     import Navbar from "../nested/navbar/navbar.svelte"
+    import Mal_win from "../nested/mal_win/mal.svelte"
+    import Quick_links from "../nested/quick_links/quick.svelte"
+    
 
 
 
