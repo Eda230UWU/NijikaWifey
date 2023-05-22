91307e0ebda74e3ff0f897ce583d0aa21ff86a92
xdIVelocity <jaros.on.2021@skola.ssps.cz>
2023-05-22 15:25:52 +0200

    notWorking

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,55 +1,53 @@
 <script>
-    import Navbar from "../nested/navbar/navbar.svelte"
-    import Mal_win from "../nested/mal_win/mal.svelte"
-    import Quick_links from "../nested/quick_links/quick.svelte"
-    import News from "../nested/news/news.svelte"
+    import Navbar from "../nested/navbar/navbar.svelte";
+    import Mal_win from "../nested/mal_win/mal.svelte";
+    import Quick_links from "../nested/quick_links/quick.svelte";
+    import News from "../nested/news/news.svelte";
+    import { Client, fetchRandom } from "nekos-best.js";
 
     export const colors_dark = [];
     export const colors_light = ["#ffffff", "#acacac"];
 
+    async function getNeko() {
+        const response = await fetch("https://nekos.best/api/v2/neko");
+        const json = await response.json();
+        console.log(json.results[0].url);
+        return json;
+    }
+    let uwu = getNeko();
 </script>
 
-<<<<<<< HEAD
-<div class="main">
-    
-=======
->>>>>>> 89991aa837dd303cf5e52f4e44f7e151c7c12f76
-
 <div class="main" style:background-color={colors_light[0]}>
-    <Navbar/>
+    <Navbar />
     <div class="content" style:background-color={colors_light[1]}>
         <div class="sidebar">
-            <Mal_win/>
-            <Quick_links/>
+            <Mal_win />
+            <Quick_links />
         </div>
         <div class="news">
-            <News/>
+            <News />
         </div>
+        {#await uwu then owo}
+        <div style="background-image=url{owo}" id="nekos">
+            <img src="{owo}" alt="">
+        </div>
+        {/await}
     </div>
-
-
-
 </div>
 
 <style lang="scss">
-<<<<<<< HEAD
-=======
     * {
         $sec: #ffffff;
         display: flex;
         flex-direction: column;
-
     }
 
-
-
     div.main {
-        
         flex-direction: column;
         align-items: center;
         width: 100vw;
         height: 100vh;
-        h1{
+        h1 {
             font-size: large;
         }
     }
@@ -60,8 +58,9 @@
         flex-direction: row;
         width: 66vw;
         height: 80vh;
-        
     }
->>>>>>> 89991aa837dd303cf5e52f4e44f7e151c7c12f76
-
+    #nekos {
+        height: 10em;
+        width: 10em;
+    }
 </style>
