588649008e417afc8a234cc3b5af9dcb1b2bc6bb
xdIVelocity <jaros.on.2021@skola.ssps.cz>
2023-05-22 13:15:19 +0200

    uwu

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,12 +9,7 @@
 </script>
 
 <div class="main">
-    <header class="navBar">
-        <div class="logo"></div>
-        <nav>
-            <div searchBar></div>
-        </nav>
-    </header>
+    
 
 
 
@@ -22,29 +17,5 @@
 </div>
 
 <style lang="scss">
-    :global(body)
-    {
-        margin: 0;
-    }
-    header{
-        padding: 0 20%;
-        display: flex;
-        justify-content: space-between;
-        height: 80px;
-        width: 60%;
-        .logo{
-            height: 80px;
-            width: 80px;
-        }
-        h3{
-              color:aqua;
-              margin: 0;  
-        }
-        nav{
-            display: flex;
-            width: 66%;
-            background-color: black;
-            margin: 10px;
-        }
-    }
+
 </style>
