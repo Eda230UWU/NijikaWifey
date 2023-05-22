6b2becc1c0b0fe541a8ef0bb64da550f2fdc2451
eda uwu <edafru@gmail.com>
2023-05-22 13:08:26 +0200

    sex

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,16 +2,25 @@
     import Navbar from "../nested/navbar/navbar.svelte"
     import Mal_win from "../nested/mal_win/mal.svelte"
     import Quick_links from "../nested/quick_links/quick.svelte"
-    
+    import News from "../nested/news/news.svelte"
 
-
+    export const colors_dark = [];
+    export const colors_light = ["#ffffff", "#acacac"];
 
 </script>
 
 
-<div class="main">
-
-
+<div class="main" style:background-color={colors_light[0]}>
+    <Navbar/>
+    <div class="content" style:background-color={colors_light[1]}>
+        <div class="sidebar">
+            <Mal_win/>
+            <Quick_links/>
+        </div>
+        <div class="news">
+            <News/>
+        </div>
+    </div>
 
 
 
@@ -19,8 +28,32 @@
 
 <style lang="scss">
     * {
+        $sec: #ffffff;
+        display: flex;
+        flex-direction: column;
 
     }
 
+
+
+    div.main {
+        
+        flex-direction: column;
+        align-items: center;
+        width: 100vw;
+        height: 100vh;
+        h1{
+            font-size: large;
+        }
+    }
+
+    div.content {
+        margin-top: 50px;
+        display: flex;
+        flex-direction: row;
+        width: 66vw;
+        height: 80vh;
+        
+    }
 
 </style>
