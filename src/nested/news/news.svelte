b1c16a8c022717db0105c613adac1f33e2b0afa3
eda uwu <edafru@gmail.com>
2023-05-23 11:13:18 +0200

    news

--- src/nested/news/news.svelte
+++ src/nested/news/news.svelte
@@ -6,6 +6,7 @@
     async function season_now() {
         const response = await fetch("https://api.jikan.moe/v4/seasons/now");
         const get_season_now = await response.json();
+        console.log(get_season_now)
         return get_season_now;
     }
 
@@ -33,22 +34,22 @@
     </div>
 
     {:then season_info}
+    <div class="animation">
     <div class="scroll">
-        
         {#each season_info.data as info}
             <div class="box1">
+                <div>
                 <h2><a href={info.url}>{info.title}</a></h2>
+                <i>Airing: {info.broadcast.day} {convert(info.broadcast.time)}</i>
                 <p>{info.score}</p>
-
+                </div>  
                 
-                    <img src={info.images.jpg.image_url} alt="image.jpg" />
+                <img src={info.images.jpg.image_url} alt="image.jpg" />
                 
-                <div class="bottom">
-                    <i>Airing at: {info.broadcast.day} {convert(info.broadcast.time)}</i>
-                </div>
+                
             </div>
         {/each}
-            
+        </div>
     </div>    
     
 {/await}
@@ -61,12 +62,15 @@
   
 
     div.scroll {
-
+        animation-name: scroll;
+        animation-duration: 60s;
+        animation-iteration-count: infinite;
         display: flex;
         flex-direction: row;
         max-height: 500px;
-        width: calc(inherit - 40px);
+        width: inherit;
         overflow: hidden;
+
     }
 
     div.ongoing {
@@ -81,7 +85,7 @@
     div.box1 {
         display: flex;
         flex-direction: column;
-        min-width: calc(20% - 10px);
+        min-width: calc(20% - 20px);
         border: 5px;
         border-color: rgb(43, 74, 74);
         justify-content: space-between;
@@ -100,6 +104,7 @@
             justify-self: center;
         }
         p {
+
             font-weight: 300;
         }
     }
@@ -108,12 +113,16 @@
         background-color: rgb(0,0,0,0);
         align-self: flex-end;
     }
-
-
     
     
     div.img img {
         align-self: center;
-        width: 100%;
+        height: 50%;
     }
+
+    
+
+    
+
+
 </style>
