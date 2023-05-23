f043e528b5452e73105c3a1216f8122af08994e9
eda uwu <edafru@gmail.com>
2023-05-23 10:36:29 +0200

    gex

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,33 +1,27 @@
 <script>
-    import Navbar from "../nested/navbar/navbar.svelte"
-    import Mal_win from "../nested/mal_win/mal.svelte"
-    import Quick_links from "../nested/quick_links/quick.svelte"
-    import News from "../nested/news/news.svelte"
-    
-    
-    
-    import { colors } from "../routes/colors.js"
+    import Navbar from "../nested/navbar/navbar.svelte";
+    import Mal_win from "../nested/mal_win/mal.svelte";
+    import Quick_links from "../nested/quick_links/quick.svelte";
+    import News from "../nested/news/news.svelte";
+
+    import { colors } from "../routes/colors.js";
     const colors_dark = colors[0].dark;
     const colors_light = colors[0].light;
-   
-
 </script>
 
-
-<div class="main" style:background-color="#ffffff">
-    <Navbar/>
+<div class="main" style:background-color={colors_light[1]}>
+    <Navbar />
     <div class="content" style:background-color="#acacac">
-        <div class="sidebar">
-            <Mal_win/>
-            <Quick_links/>
-        </div>
-        <div class="news">
-            <News/>
+        <div class="flex">
+            <div class="sidebar">
+                <Mal_win />
+                <Quick_links />
+            </div>
+            <div class="news">
+                <News />
+            </div>
         </div>
     </div>
-
-
-
 </div>
 
 <style lang="scss">
@@ -35,38 +29,40 @@
         $sec: #ffffff;
         display: flex;
         flex-direction: column;
-
     }
-
-
 
     div.main {
-        
+        min-width: 1000px;
         flex-direction: column;
         align-items: center;
-        width: 100vw;
+        max-width: 100vw;
         height: 100vh;
-        
     }
-    
+
+    div.flex {
+        display: flex;
+        height: 100%;
+        flex-direction: row;
+    }
+
     div.content {
         margin-top: 50px;
-        display: flex;
-        flex-direction: row;
-        width: 80vw;
-        height: 80vh;
-        min-width: 1000px;
+        margin: 50px auto 20px auto;
+        max-width: 80vw;
+        padding: 10px;
         
     }
     div.sidebar {
-        flex-grow: 1;
+        
         min-width: 150px;
     }
-    
-    div.news {
-        margin-left: 90px;
-        flex-grow: 2.5;
-        padding: 20px;
-    }
 
+
+    div.news {
+        display: block;
+        overflow: hidden;
+        margin-left: 90px;
+        padding: 20px;
+        max-width: 100%;
+    }
 </style>
